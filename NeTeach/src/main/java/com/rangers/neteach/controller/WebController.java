139a8619082ecdcc1d73933fa208545e7bf8f8e4
angela <angela@DESKTOP-TNBKG30>
2021-01-16 13:32:17 -0300

    metodos de busqueda implementados en controlador

--- NeTeach/src/main/java/com/rangers/neteach/controller/WebController.java
+++ NeTeach/src/main/java/com/rangers/neteach/controller/WebController.java
@@ -2,8 +2,10 @@ package com.rangers.neteach.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rangers.neteach.modelo.Testimonio;
 import com.rangers.neteach.modelo.Usuario;
@@ -52,6 +54,43 @@ public class WebController {
 	@GetMapping("/tutores") 
 	public String tutores() { 
 		  return "tutores"; 
+	}
+	
+	
+	/* FILTROS DE BUSQUEDA */
+	@PostMapping("buscarNombre")
+	public String buscarNombre(String nombre, ModelMap model) {
+		model.put("usuarios", repUsuario.findByNombre(nombre));
+
+		return "tutores";
+	}
+	
+	@PostMapping("buscarApellido")
+	public String buscarApellido(String apellido, ModelMap model) {
+		model.put("usuarios", repUsuario.findByApellido(apellido));
+
+		return "tutores";
+	}
+	
+	@PostMapping("buscarArea")
+	public String buscarArea(String materia, ModelMap model) {
+		model.put("usuarios", repUsuario.findByMateria(materia));
+
+		return "tutores";
+	}
+	
+	@PostMapping("buscarSexo")
+	public String buscarSexo(String sexo, ModelMap model) {
+		model.put("usuarios", repUsuario.findBySexo(sexo));
+
+		return "tutores";
+	}
+	
+	@PostMapping("buscarEdad")
+	public String buscarNombre(int edad, ModelMap model) {
+		model.put("usuarios", repUsuario.findByEdad(edad));
+
+		return "tutores";
 	}
 	
 	
