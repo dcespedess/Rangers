3b726d7d9de934d732a7b39c72d0331c9a72b3b9
Miku <Miku@192.168.0.20>
2021-01-16 00:39:29 -0300

    agregado navbar

--- NeTeach/src/main/webapp/resources/js/script.js
+++ NeTeach/src/main/webapp/resources/js/script.js
@@ -1,3 +1,10 @@
-/**
- * 
- */
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var resp = document.getElementById("navbar");
+    if (resp.className === "navbar") {
+      resp.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
