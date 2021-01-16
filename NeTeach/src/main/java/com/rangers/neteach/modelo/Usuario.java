a87913bc18e1341bf0a7ae37bb822f81aabe672a
angela <angela@DESKTOP-TNBKG30>
2021-01-16 13:19:51 -0300

    Filtros de busqueda y clase Usuario modificada

--- NeTeach/src/main/java/com/rangers/neteach/modelo/Usuario.java
+++ NeTeach/src/main/java/com/rangers/neteach/modelo/Usuario.java
@@ -18,6 +18,8 @@ public class Usuario {
 	
 	private String apellido;
 	
+	private String materia;
+	
 	@Min(18)
 	@Max(99)
 	private int edad;
@@ -27,97 +29,89 @@ public class Usuario {
 	
 	@Column (length = 100)
 	private String descUsuario;
-	
-	
-	public Usuario(int id, String nombre, String apellido, int edad, String sexo, String descUsuario) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.edad = edad;
-		this.sexo = sexo;
-		this.descUsuario = descUsuario;
-	}
-
 
 	public Usuario() {
 		super();
 	}
 
-
-
-	public Usuario(int id) {
+	public Usuario(int id, String nombre, String apellido, String materia, @Min(18) @Max(99) int edad, String sexo,
+			String descUsuario) {
 		super();
 		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.materia = materia;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.descUsuario = descUsuario;
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 
 	public String getApellido() {
 		return apellido;
 	}
 
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
+	public String getMateria() {
+		return materia;
+	}
+
+	public void setMateria(String materia) {
+		this.materia = materia;
+	}
 
 	public int getEdad() {
 		return edad;
 	}
 
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
 
 	public String getSexo() {
 		return sexo;
 	}
 
-
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-
 
 	public String getDescUsuario() {
 		return descUsuario;
 	}
 
-
 	public void setDescUsuario(String descUsuario) {
 		this.descUsuario = descUsuario;
 	}
 
-
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", sexo="
-				+ sexo + ", descUsuario=" + descUsuario + "]";
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + ", materia=" + materia
+				+ ", edad=" + edad + ", sexo=" + sexo + ", descUsuario=" + descUsuario + "]";
 	}
 	
+	
+
+
+
 	
 	
 
--- NeTeach/src/main/java/com/rangers/neteach/repository/IUsuarioRepository.java
+++ NeTeach/src/main/java/com/rangers/neteach/repository/IUsuarioRepository.java
@@ -1,8 +1,16 @@
 package com.rangers.neteach.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rangers.neteach.modelo.Usuario;
 
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+	 List<Usuario> findByNombre (String nombre);
+	 List<Usuario> findByApellido (String apellido);
+	 List<Usuario> findByMateria (String materia);
+	 List<Usuario> findBySexo (String sexo);
+	 List<Usuario> findByEdad (int edad);
+	 
 
 }
